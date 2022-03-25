c25791d9b94b56988cea80d15a37d415b2586e01
kaanuyn <kaanuyann@gmail.com>
2022-03-25 21:16:28 +0300

    first commit

---
+++ Assets/Scripts/Weapons/AmmoDisplay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmmoDisplay : MonoBehaviour
+{
+
+    public GameObject ammoTextUI;
+    public int pistolCount;
+
+    void Update()
+    {
+        ammoTextUI.GetComponent<Text>().text = "Ammo: " + pistolCount;
+    }
+}
